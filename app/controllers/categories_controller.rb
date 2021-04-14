c39eb8201b960b2f60a16e07419cf43006dddabd
HaidyF <69495135+HaidyF@users.noreply.github.com>
2021-04-14 15:26:35 -0400

    database

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,30 +4,4 @@ class CategoriesController < ApplicationController
 
         render json: @categories
     end
-
-    def show
-        render json: @category
-    end
-
-    def create
-        @category = Category.new(category_params)
-
-        if @category.save
-            render json: @category
-        else
-            render json: @category.errors
-        end
-    end
-
-    def update
-        if @category.update(category_params)
-            render json: @category
-        else
-            render json: @category.errors
-        end
-    end
-
-    def delete
-
-    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-residential = Category.create{name= "Residential"}
-commercial = Category.create{name= "Commercial"}
-familyHouse = Category.create{name= "Family House"}
-mixedUse = Category.create{name= "Mixed Use"}
+residential = Category.create(name: 'Residential')
+commercial = Category.create(name: 'Commercial')
+familyHouse = Category.create(name: 'Family House')
+mixedUse = Category.create(name: 'Mixed Use')
 
 # project1 = Project.create{title= "First Project"}
-project1 = Project.create{title: "First Project", quote: "Something Inspirational", description: "A Project", image_URL: "2wCEAAoHCBYWFRgWFhUYGRgYHBgaGhgaGhoZGhgYGhgZGhgYGhwcIS4lHB4rIRgYJjgmKy8xNTU1GiQ7QDs0Py40NTEBDAwMEA8QHhISHjQhJCQ0NDQ0NDQ0NDQ0NDQ0NjQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDE0NDQ0NDQ0NDQ0NDQ0NP",location: "NYC", year: "2021", category_id: commercial.id}
+project1 = Project.create(title: "First Project", quote: "Something Inspirational", description: "A Project", image_URL: "2wCEAAoHCBYWFRgWFhUYGRgYHBgaGhgaGhoZGhgYGhgZGhgYGhwcIS4lHB4rIRgYJjgmKy8xNTU1GiQ7QDs0Py40NTEBDAwMEA8QHhISHjQhJCQ0NDQ0NDQ0NDQ0NDQ0NjQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDE0NDQ0NDQ0NDQ0NDQ0NP",location: "NYC", year: "2021", category_id: commercial.id)
